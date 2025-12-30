b8e35b447deea85fb12f6f68f7a07c28a0d1b28d
ShivashanShivashankar Sanadi <shivashankarsanadi@gmail.com>
2025-12-30 14:27:07 +0530

    Deploy React app to GitHub Pages

--- Frontend/food/src/index.js
+++ Frontend/food/src/index.js
@@ -1,47 +1,23 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import {BrowserRouter} from 'react-router-dom';
-// import reportWebVitals from './reportWebVitals';
-
-// import CartState from "./components/AddToCart/context/cart/CartState";
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <BrowserRouter>
-//   <React.StrictMode>
-  
-//   <CartState>
-//     <App />
-//     </CartState>
-//   </React.StrictMode>
-//  </BrowserRouter>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
 
 
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import CartState from './components/AddToCart/context/cart/CartState';
 import CartProvider from './components/Cart';
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CartState>
         <CartProvider>
           <App />
         </CartProvider>
       </CartState>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
